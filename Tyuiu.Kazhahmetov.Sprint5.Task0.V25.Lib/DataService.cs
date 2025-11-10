39c2d9c0f3a8ad5b3610816fc559ccb4f7d90398
Таджикович Ержан <kazamircik@gmail.com>
2025-11-10 17:11:05 +0500

    Task 0

--- Tyuiu.Kazhahmetov.Sprint5.Task0.V25.Lib/DataService.cs
+++ Tyuiu.Kazhahmetov.Sprint5.Task0.V25.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.Kazhahmetov.Sprint5.Task0.V25.Lib
         public string SaveToFileTextData(int x)
         {
             string noway = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            double res = ((3 * Math.Pow(x, 4)) + 1) / Math.Pow(x, 3);
+            double res = Math.Round((((3 * Math.Pow(x, 4)) + 1) / Math.Pow(x, 3)), 3);
             File.WriteAllText(noway, Convert.ToString(res));
             return noway;
         }
