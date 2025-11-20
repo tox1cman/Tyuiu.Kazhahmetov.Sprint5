13a019094b3cbed9ab7d93724da7146be469148c
Таджикович Ержан <kazamircik@gmail.com>
2025-11-20 22:28:55 +0500

    Task 6

--- Tyuiu.Kazhahmetov.Sprint5.Task6.V27.Lib/DataService.cs
+++ Tyuiu.Kazhahmetov.Sprint5.Task6.V27.Lib/DataService.cs
@@ -5,7 +5,16 @@ namespace Tyuiu.Kazhahmetov.Sprint5.Task6.V27.Lib
     {
         public int LoadFromDataFile(string path)
         {
-            
+            int count = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
