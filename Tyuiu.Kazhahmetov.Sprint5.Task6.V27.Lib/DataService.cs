3b675649829b71b2931f14fd795ac4d03684efc7
Таджикович Ержан <kazamircik@gmail.com>
2025-11-20 22:26:25 +0500

    Task 6

---
+++ Tyuiu.Kazhahmetov.Sprint5.Task6.V27.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.Kazhahmetov.Sprint5.Task6.V27.Lib
+{
+    public class DataService : ISprint5Task6V27
+    {
+        public int LoadFromDataFile(string path)
+        {
+            
+        }
+    }
+}
